c660d33afcad7f857c19bd020addb19c817dfd94
Phuc-215 <ntphuc23@clc.fitus.edu.vn>
2025-12-16 17:25:48 +0700

    [FE][feat] Set the scroll position to top of the page after navigation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import ProtectedRoute from './components/ProtectedRoute';
 import AuthLayout from './layouts/AuthLayout';
 import MainLayout from './layouts/MainLayout';
@@ -16,44 +16,42 @@ import NotFound from './pages/NotFound';
 
 function App() {
   return (
-    <Router>
-      <div className='min-h-screen transition-colors duration-100 bg-[var(--bg)] color-[var(--text)]'>
-        <Routes>
-          {/* Auth Routes */}
-          <Route element={<AuthLayout />}>
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
+    <div className='min-h-screen transition-colors duration-100 bg-[var(--bg)] color-[var(--text)]'>
+      <Routes>
+        {/* Auth Routes */}
+        <Route element={<AuthLayout />}>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Route>
+        
+        <Route element={<MainLayout />}>
+          {/* Public Routes */}
+          <Route path="/" element={<HomePage />} />
+          <Route path="/search" element={<ListProducts />} />
+          <Route path="/auction/:id" element={<ProductDetail />} />
+
+          {/* --- LEVEL 1: STANDARD USERS (Just need to be logged in) --- */}
+          <Route element={<ProtectedRoute />}>
+            <Route path="/me" element={<UserDashboard />} />
+            <Route path="/me/:variant" element={<ViewAll />} />
+            <Route path="/profile/:id" element={<PublicProfile />} />
           </Route>
-          
-          <Route element={<MainLayout />}>
-            {/* Public Routes */}
-            <Route path="/" element={<HomePage />} />
-            <Route path="/search" element={<ListProducts />} />
-            <Route path="/auction/:id" element={<ProductDetail />} />
 
-            {/* --- LEVEL 1: STANDARD USERS (Just need to be logged in) --- */}
-            <Route element={<ProtectedRoute />}>
-              <Route path="/me" element={<UserDashboard />} />
-              <Route path="/me/:variant" element={<ViewAll />} />
-              <Route path="/profile/:id" element={<PublicProfile />} />
-            </Route>
-
-            {/* --- LEVEL 2: SELLER ONLY (Needs 'seller' role) --- */}
-            <Route element={<ProtectedRoute requiredRole="seller" />}>
-              <Route path="/create" element={<CreateAuction />} />
-            </Route>
-
-            {/* --- LEVEL 3: ADMIN ONLY (Needs 'admin' role) --- */}
-            <Route element={<ProtectedRoute requiredRole="admin" />}>
-              <Route path="/admin" element={<AdminDashboard />} />
-            </Route>
-
-            {/* Fallback Route */}
-            <Route path="*" element={<NotFound />} />
+          {/* --- LEVEL 2: SELLER ONLY (Needs 'seller' role) --- */}
+          <Route element={<ProtectedRoute requiredRole="seller" />}>
+            <Route path="/create-auction" element={<CreateAuction />} />
           </Route>
-        </Routes>
-      </div>
-    </Router>
+
+          {/* --- LEVEL 3: ADMIN ONLY (Needs 'admin' role) --- */}
+          <Route element={<ProtectedRoute requiredRole="admin" />}>
+            <Route path="/admin" element={<AdminDashboard />} />
+          </Route>
+
+          {/* Fallback Route */}
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
+    </div>
   )
 }
 
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,16 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from './contexts/ThemeContext';
 import { AuthProvider } from './contexts/AuthContext.jsx';
+import ScrollToTop from './components/ScrollToTop.jsx'
 import App from './App.jsx'
 import './styles/main.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <ThemeProvider>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </ThemeProvider>
+    <BrowserRouter>
+      <ScrollToTop />
+      <ThemeProvider>
+        <AuthProvider>
+          <App />
+        </AuthProvider>
+      </ThemeProvider>
+    </BrowserRouter>
   </StrictMode>,
 )
